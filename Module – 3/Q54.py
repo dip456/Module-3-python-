e3b4fde6ba4e3d8a5cd3f562b1b24eddf7ff9c47
dip456 <148782458+dip456@users.noreply.github.com>
2024-02-28 11:01:05 +0530

    Update Q54.py

--- Module – 3/Q54.py
+++ Module – 3/Q54.py
@@ -1,24 +1,8 @@
-""" How can you pick a random item from a range? """
-"""
+"""How can you pick a random item from a range? """
+print(' 1 to 50:')
 
+x=random.randrange(1,50)
+print(x)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
+y=random.randrange(1,50,2)
+print(y)
