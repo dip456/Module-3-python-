6d9800e325c373f2187f67ae3bc54920142a91ff
dip456 <148782458+dip456@users.noreply.github.com>
2024-02-28 10:50:57 +0530

    Update Q58.py

--- Module – 3/Q58.py
+++ Module – 3/Q58.py
@@ -1,25 +1,6 @@
-""" Write a Python program to read a random line from a file. 
-"""
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
+""" Write a Python program to read a random line from a file. """
+import random
+def r_line(fname):
+    lines = open(fname).read().splitlines()
+    return random.choice(lines)
+print(r_line('test.txt'))
