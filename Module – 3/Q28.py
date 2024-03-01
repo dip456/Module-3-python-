9638096fe5262da7a849e7991cc2faf5da9b7997
dip456 <148782458+dip456@users.noreply.github.com>
2024-03-01 17:26:31 +0530

    Update Q28.py

--- Module – 3/Q28.py
+++ Module – 3/Q28.py
@@ -1,9 +1,11 @@
 """ Write a Python program to remove an empty tuple(s) from a list of tuples. """
-def remove_empty_tuples(tuples_list):
-   
-    return [i for i in tuples_list if i]
-
-original_list = [(1, 2, 3), (), (4, 5), (), (6,), ()]
-modified_list = remove_empty_tuples(original_list)
-print("Original list:", original_list)
-print("Modified list:", modified_list)
+def remove_empty(tuples_list):
+    res = []
+    for i in tuples_list:
+        if i:
+            res.append(i)
+    return res
+list = [(1, 2, 3), (), (4, 5), (), (6), ()]
+new_list = remove_empty(list)
+print("original list:",list)
+print("modified list:",new_list)
