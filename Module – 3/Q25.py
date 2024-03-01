22ebb4e7b496fc29a2b18e6413b418632d028f8a
dip456 <148782458+dip456@users.noreply.github.com>
2024-03-01 18:15:31 +0530

    Update Q25.py

--- Module – 3/Q25.py
+++ Module – 3/Q25.py
@@ -1,6 +1,7 @@
 """Write a Python program to reverse a tuple. """
-def rev_tuple(tuples):
-    rev = tuples[::-1]
+def rev_tuple(my_tuple):
+    rev = tuple(reversed(my_tuple))
     return rev
-my_tuple = (10,20,30,40,50,60)
-print(rev_tuple(my_tuple))
+tuple_list = (10,20,30,50)
+new_tuple = rev_tuple(tuple_list)
+print("reversed : ",new_tuple)
