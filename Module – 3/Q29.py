04d52147fb2614bb5b33854429d8bd46c9e65899
dip456 <148782458+dip456@users.noreply.github.com>
2024-03-01 17:16:08 +0530

    Update Q29.py

--- Module – 3/Q29.py
+++ Module – 3/Q29.py
@@ -1,7 +1,7 @@
 """ Write a Python program to unzip a list of tuples into individual lists. """
 def unzip_list(list_tuples):
     
-    return tuple(zip(*list_tuples))
+    return list(zip(*list_tuples))
 
 list_t = [(1, 2, 3), ('dipak', 'sanket', 'kiran'), (True, False, True)]
 unzip_lists = unzip_list(list_t)
