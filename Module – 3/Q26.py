72495ce85ea1bee356f91081169b3dc8603abd07
dip456 <148782458+dip456@users.noreply.github.com>
2024-03-01 17:53:33 +0530

    Update Q26.py

--- Module – 3/Q26.py
+++ Module – 3/Q26.py
@@ -1,10 +1,12 @@
 """ Write a Python program to replace last value of tuples in a list. """
-def change_last(tuples_list, new_value):
-    
-    return [(i[:-1] + (my_value,)) for i in tuples_list]
+def change_last(tuple_list, new_value):
+    new_list = []
+    for i in tuple_list:
+        new_tuple = i[:-1] + (new_value,)
+        new_list.append(new_tuple)
+    return new_list
 
-list = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+list_tuples = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
 my_value = 100
-modified_list = change_last(list, my_value)
-#print("Original list:",list)
-print("Modified list:", modified_list)
+modifie_list = change_last(list_tuples, my_value)
+print("modifie list:", modifie_list)
