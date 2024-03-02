58006e79a78940fb8a837754105dbdecf6afd6bb
dip456 <148782458+dip456@users.noreply.github.com>
2024-03-02 11:01:29 +0530

    Update Q50.py

--- Module – 3/Q50.py
+++ Module – 3/Q50.py
@@ -1,5 +1,5 @@
 """ Write a Python function to check whether a number is perfect or not. """
-def is_perfect(number):
+def perfect_n(number):
    
     if number <= 0:
         return False
@@ -7,6 +7,5 @@ def is_perfect(number):
     divisor_sum = sum([divisor for divisor in range(1, number) if number % divisor == 0])
     return divisor_sum == number
 
-# Test the function
 number = 28
-print("Is", number, "a perfect number?", is_perfect(number))
+print(perfect_n(number))
