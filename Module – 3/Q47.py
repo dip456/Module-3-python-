e6a7809b1754295595fe06020fb4c6ec96450e49
dip456 <148782458+dip456@users.noreply.github.com>
2024-03-02 11:22:37 +0530

    Update Q47.py

--- Module – 3/Q47.py
+++ Module – 3/Q47.py
@@ -4,7 +4,7 @@ Note: Track the count of the letters from the string.
 Sample string: 'w3resource' 
 Expected output: 
 {'3': 1,’s’: 1, 'r': 2, 'u': 1, 'w': 1, 'c': 1, 'e': 2, 'o': 1}  """
-def create_dictionary_from_string(input_string):
+def create_dict(input_string):
     char_count = {}
     
     for char in input_string:
@@ -12,9 +12,7 @@ def create_dictionary_from_string(input_string):
     
     return char_count
 
-# Sample string
-input_string = 'w3resource'
+input_string = 'DipakChaudhari'
 
-# Create dictionary from the string
-result = create_dictionary_from_string(input_string)
-print("Dictionary created from the string:", result)
+result = create_dict(input_string)
+print("Dict created from the string:", result)
