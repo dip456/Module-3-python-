2be3e3a7f6eae0620414ea837994d8a330947fd5
dip456 <148782458+dip456@users.noreply.github.com>
2024-03-02 10:34:27 +0530

    Update Q41.py

--- Module – 3/Q41.py
+++ Module – 3/Q41.py
@@ -4,7 +4,6 @@ Sample output: Counter ({'a': 400, 'b': 400,’d’: 400, 'c': 300}). """
 
 from collections import Counter
 
-# Create two dictionaries 'd1' and 'd2' with key-value pairs.
 d1 = {'a': 100, 'b': 200, 'c': 300}
 d2 = {'a': 300, 'b': 200, 'd': 400}
 
